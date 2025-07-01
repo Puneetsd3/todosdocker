8aac76b46e27e3de28d70874b230f68207420ace
Puneet <170717126+Puneetsd3@users.noreply.github.com>
2025-07-01 14:55:17 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -77,15 +77,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 REST_FRAMEWORK = {
