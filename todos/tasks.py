efd55d9639ae667906d382a3aaf464ef074dcff9
Puneet <170717126+Puneetsd3@users.noreply.github.com>
2025-07-02 15:42:23 +0530

    Update tasks.py

--- todos/tasks.py
+++ todos/tasks.py
@@ -5,7 +5,7 @@ from django.conf import settings
 @shared_task
 def send_todo_completion_email(user_email, todo_title):
     subject = f'Todo Completed: {todo_title}'
-    message = f'Congratulations! Your todo "todo_title" has been marked as completed.'
+    message = f'Congratulations!! Your todo "todo_title" has been marked as completed.'
     send_mail(
         subject,
         message,
